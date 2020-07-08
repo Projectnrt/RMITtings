865f7c67c4be645dc700ec06e629438a011c5835
Projectnrt <Projectnrt@gmail.com>
2020-07-08 10:41:54 +1000

    Update SHATS.js

--- SHATS.js
+++ SHATS.js
@@ -1,3 +1,4 @@
+//Created by Eneti Waretini
 //Modal =====================================================================================
 var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
